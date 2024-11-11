d0ef6e084bc4ebb420b9a97b9f09a9a5e0052e46
sammou00 <sammoustafa00@gmail.com>
2024-11-11 17:00:13 +0100

    update utils

--- utils/validatePassword.js
+++ utils/validatePassword.js
@@ -1,11 +1,8 @@
 const validatePassword = (password) => {
     const pattern =
         /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,16}$/;
-    if (pattern.test(password)) {
-        return true;
-    } else {
-        return false;
-    }
+        
+    return pattern.test(password);
 };
 
 export default validatePassword;
